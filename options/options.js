137554cde195a3e410db92644868a3189068cef1
Crowdbotics <team@crowdbotics.com>
2023-08-30 15:35:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delly-foods-deliver-43148",
+  url: "https://delly-foods-deliver-43148.botics.co",
+  api: "https://delly-foods-deliver-43148.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
